c374b3f5d82abf1282ebb1646836777826fcf502
Matěj Šimůnek <matej-simunek@seznam.cz>
2026-09-17 11:52:03 +0200

    Add x-tools for BBB and BBAI64

---
+++ aarch64-unknown-linux-gnu/lib/gcc/aarch64-unknown-linux-gnu/13.4.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/matej/Work/crosstool-ng/.build/aarch64-unknown-linux-gnu/src/gcc/configure --build=x86_64-build_pc-linux-gnu --host=x86_64-build_pc-linux-gnu --target=aarch64-unknown-linux-gnu --prefix=/home/matej/x-tools/aarch64-unknown-linux-gnu --exec_prefix=/home/matej/x-tools/aarch64-unknown-linux-gnu --with-sysroot=/home/matej/x-tools/aarch64-unknown-linux-gnu/aarch64-unknown-linux-gnu/sysroot --enable-languages=c,c++ --with-pkgversion='crosstool-NG 1.28.0.58_27cd838' --enable-__cxa_atexit --disable-libmudflap --disable-libgomp --disable-libssp --disable-libquadmath --disable-libquadmath-support --disable-libsanitizer --disable-libmpx --with-gmp=/home/matej/Work/crosstool-ng/.build/aarch64-unknown-linux-gnu/buildtools --with-mpfr=/home/matej/Work/crosstool-ng/.build/aarch64-unknown-linux-gnu/buildtools --with-mpc=/home/matej/Work/crosstool-ng/.build/aarch64-unknown-linux-gnu/buildtools --with-isl=/home/matej/Work/crosstool-ng/.build/aarch64-unknown-linux-gnu/buildtools --enable-lto --enable-threads=posix --enable-target-optspace --enable-plugin --disable-nls --disable-multilib --with-local-prefix=/home/matej/x-tools/aarch64-unknown-linux-gnu/aarch64-unknown-linux-gnu/sysroot --enable-long-long";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
